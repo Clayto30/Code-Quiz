ca92e0f4658e469a00f7b15a26a6677a6d07b1f0
Clayton Goff <cltngoff@gmail.com>
2021-02-28 18:39:51 -0800

    iterates through questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,42 +3,46 @@ var currentQuestion = 0
 var answersEl = document.getElementById("answers")
 let time = 120
 var countdownEl = document.getElementById("countdown")
-//var buttonEl = document.getElementsByName("button")
-
-var addCountdownEl = document.createElement("p");
-    addCountdownEl.textContent = "Time: " + time;
+var lessTime = function() {time--, addCountdownEl.textContent = "Time: " + time;
     countdownEl.appendChild (addCountdownEl);
-// array with all my questions 
+    }
+var countingDown = setInterval(lessTime, 1000);
+var addCountdownEl = document.createElement("p");
+var gradeEl = document.getElementById("grade")
+var addGradeEl = document.createElement("h1");
+
+
+// array with questions and answers 
 let questions = [{
     question : "Commonly used data types DO Not Include:", 
     answers : ["strings", "booleans", "alerts", "numbers"],
-    correctAnswer : [2]
+    correctAnswer : "alerts"
 },
 {
     question : "The condition in an if / else statement is enclosed with _________.", 
     answers : ["quotes", "curly brackets", "parenthesis", "square brackets"],
-    correctAnswer : [2]
+    correctAnswer : "parenthesis"
 },
     {
     question : "Arrays in JavaScript can be used to store ________.", 
     answers : ["numbers and strings", "other arrays", "booleans", "all of the above"],
-    correctAnswer : [3]
+    correctAnswer : "all of the above"
 },
 {
     question : "String values must be enclosed within _______ when being assigned to variables.", 
     answers : ["commas", "curly brackets", "quotes", "parenthesis"],
-    correctAnswer : [2]
+    correctAnswer : "quotes"
 },
 {
     question : "A very useful tool used during development and debugging for printing content to the debugger is:", 
     answers : ["JavaScript", "terminal/bash", "for loops", "console.log"],
-    correctAnswer : [3]
+    correctAnswer : "console.log"
 }];
 
 displayQuestion = function () {
-    // clear the old question containerElement.innerHtml = "" for both questions and answers
-   // questionsEl.innerHTML = ""; //this is an attempt to match above
-    // answersEl.innerHTML = ""; // this is an attempt to match above pseudocode
+    // clear the old question 
+   questionsEl.innerHTML = "";
+    answersEl.innerHTML = ""; 
    // display question to user
    var addQuestionEl = document.createElement("h1");
    addQuestionEl.textContent = questions[currentQuestion].question;
@@ -51,48 +55,60 @@ displayQuestion = function () {
         newButton.textContent = questions[currentQuestion].answers[i];
         answersEl.appendChild (newButton);
 
-
         // buttons call "submit answer when clicked" on click submit answer 
-        // addEventListener.newButton(click, submitAnswer); // not really working, I'm sure.
+        newButton.addEventListener("click", submitAnswer); // not really working, I'm sure.
         }
    // button should know what answer should be , newButton.dataType.answer = "yourAnswer"
 }
+var submitAnswer = function () {
+    console.log(this);
+    if (this === questions[currentQuestion].correctAnswer) {
+        gradeEl.textContent = "Correct!"
+        gradeEl.appendChild (addGradeEl);
+        time += 15;
+    }
+    else if (this !== questions[currentQuestion].correctAnswer) {
+        gradeEl.textContent = "Wrong!"
+        gradeEl.appendChild (addGradeEl);
+        time -= 10;
+
+    }
 
 //submitAnswer = function () {
     // check and see if answer submmitted is correct, if not, take sand out of hourglass, this.dataType.answer should get me the value I'm looking for , if answer is = current index answer... 
-    //this.dataType.yourAnswer;
+    //this.dataType.answer;
     //console.log(this.dataType.yourAnswer);
-    //if (this.dataType.yourAnswer === questions[currentQuestion].correctAnswer) {
+    //if (this.dataType.answer === questions[currentQuestion].correctAnswer) {
        // console.log("correct!")
     // if the timer is zero, end quiz
+   // if (time <= 0) {endQuiz};
    // if (yourAnswer !== questions[currentQuestion].correctAnswer) {
        // time -= 15;
    // }
-    //currentQuestion++;
+    if (time > 0) {
+        currentQuestion++;
+    }
    // if (currentQuestion <= questions.length) {
-      //  displayQuestion;
-    //}
+    if (currentQuestion <= questions.length) {
+        displayQuestion();
+    }
     // if the timer is not zero , increment to next question in array, and display next question to user
     
 
-
+}
 //}
 
-//var countdown = function() {
-  //  var timeDec = timeDecrement() { 
-    //    time--;
-    //}
-    //setInterval(timeDec, 1000);
-    //};
-    //countdown();
-//console.log(time);
+
 
 //dynamically create question
 displayQuestion();
 
+
 // Below this is stuff I tried to get from class
 // if score equals zero or we win, we call this endQuiz function
-//function endQuiz() {
+var endQuiz = function() {
+
+}
    // initialEl.innerText = "";
     //questionsEl.innerText = "Enter your initials";
   //  var newTextField = document.createElement("input");
@@ -106,4 +122,4 @@ displayQuestion();
    // var name = document.getElementById("initials").value;
     //leaderBoard = [name, score] // score might change to time or timeLeft
 
-    //localStorage.setItem('leaderBoard', JSON.stringify(leaderboard))};
+    //localStorage.setItem('leaderBoard', JSON.stringify(leaderboard))}
