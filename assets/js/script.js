001f5dba104acd116354d493221e5fca4de2e24c
Clayton Goff <cltngoff@gmail.com>
2021-02-28 16:49:20 -0800

    time added to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
-// global variables 
-var questionsArr = [question1, question2, question3]
-var question1 = "what?"
-var question2 = "why?"
-var question3 = "who?"
 var questionsEl = document.getElementById("questions")
 var currentQuestion = 0
 var answersEl = document.getElementById("answers")
+let time = 120
+var countdownEl = document.getElementById("countdown")
 //var buttonEl = document.getElementsByName("button")
-//let time = 120;
+
+var addCountdownEl = document.createElement("p");
+    addCountdownEl.textContent = "Time: " + time;
+    countdownEl.appendChild (addCountdownEl);
 // array with all my questions 
 let questions = [{
     question : "Commonly used data types DO Not Include:", 
@@ -30,13 +30,15 @@ let questions = [{
     correctAnswer : [2]
 },
 {
-    question : "A very useful too used during development and debugging for printing content to the debugger is:", 
+    question : "A very useful tool used during development and debugging for printing content to the debugger is:", 
     answers : ["JavaScript", "terminal/bash", "for loops", "console.log"],
     correctAnswer : [3]
-}]
+}];
 
 displayQuestion = function () {
     // clear the old question containerElement.innerHtml = "" for both questions and answers
+   // questionsEl.innerHTML = ""; //this is an attempt to match above
+    // answersEl.innerHTML = ""; // this is an attempt to match above pseudocode
    // display question to user
    var addQuestionEl = document.createElement("h1");
    addQuestionEl.textContent = questions[currentQuestion].question;
@@ -51,29 +53,30 @@ displayQuestion = function () {
 
 
         // buttons call "submit answer when clicked" on click submit answer 
-        
+        // addEventListener.newButton(click, submitAnswer); // not really working, I'm sure.
         }
    // button should know what answer should be , newButton.dataType.answer = "yourAnswer"
 }
-submitAnswer = function () {
+
+//submitAnswer = function () {
     // check and see if answer submmitted is correct, if not, take sand out of hourglass, this.dataType.answer should get me the value I'm looking for , if answer is = current index answer... 
-    this.dataType.yourAnswer;
-    console.log(this.dataType.yourAnswer);
-    if (this.dataType.yourAnswer === questions[currentQuestion].correctAnswer) {
-        console.log("correct!")
+    //this.dataType.yourAnswer;
+    //console.log(this.dataType.yourAnswer);
+    //if (this.dataType.yourAnswer === questions[currentQuestion].correctAnswer) {
+       // console.log("correct!")
     // if the timer is zero, end quiz
-    if (yourAnswer !== questions[currentQuestion].correctAnswer) {
-        time -= 15;
-    }
-    currentQuestion++;
-    if (currentQuestion <= questions.length) {
-        displayQuestion;
-    }
+   // if (yourAnswer !== questions[currentQuestion].correctAnswer) {
+       // time -= 15;
+   // }
+    //currentQuestion++;
+   // if (currentQuestion <= questions.length) {
+      //  displayQuestion;
+    //}
     // if the timer is not zero , increment to next question in array, and display next question to user
     
 
 
-}
+//}
 
 //var countdown = function() {
   //  var timeDec = timeDecrement() { 
@@ -85,10 +88,22 @@ submitAnswer = function () {
 //console.log(time);
 
 //dynamically create question
-displayQuestion()
-// event.listen
-// right or wrong
-// i++
+displayQuestion();
 
-// stuff from TA working with ianosborne
+// Below this is stuff I tried to get from class
+// if score equals zero or we win, we call this endQuiz function
+//function endQuiz() {
+   // initialEl.innerText = "";
+    //questionsEl.innerText = "Enter your initials";
+  //  var newTextField = document.createElement("input");
+  //  newTextField.id = "initials";
+   // var submitButton = document.createElement("button");
+  //  submitButton.addEventListener("click", saveScore);
+    //questionEl.appendChild(newTextField);
+    //
 
+//function saveScore() {
+   // var name = document.getElementById("initials").value;
+    //leaderBoard = [name, score] // score might change to time or timeLeft
+
+    //localStorage.setItem('leaderBoard', JSON.stringify(leaderboard))};
